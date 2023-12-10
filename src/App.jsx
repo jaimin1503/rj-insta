5c1b38a9e53358b6e31201532717853b6208734f
jaimin1503 <123434935+jaimin1503@users.noreply.github.com>
2023-12-10 16:33:18 +0530

    signin signup form

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,15 @@
+import SignIn from "./Forms/SignIn";
+import SignUp from "./Forms/SignUp";
+import { Route, Routes } from "react-router-dom";
+
 function App() {
-  return <div className=" text-xl">Hello People!</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/signin" element={<SignIn />}></Route>
+        <Route path="/signup" element={<SignUp />}></Route>
+      </Routes>
+    </div>
+  );
 }
 export default App;
