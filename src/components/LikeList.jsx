a635519c58642d55761f92572e619e210bc5da5f
jaimin1503 <123434935+jaimin1503@users.noreply.github.com>
2023-12-22 09:11:24 +0530

    show liked list complete

--- src/components/LikeList.jsx
+++ src/components/LikeList.jsx
@@ -1,6 +1,59 @@
-const LikeList = () => {
+import { Close } from "@mui/icons-material";
+import jatu from "./assets/jatuu.jpg";
+import { useEffect, useState } from "react";
+const LikeList = ({ show, post }) => {
+  const [likes, setLikes] = useState([]);
+  useEffect(() => {
+    setLikes(post.like);
+  }, []);
   return (
-	<div>LikeList</div>
-  )
-}
-export default LikeList
+    <div>
+      <div
+        style={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+        }}
+        className="container h-[50vh] w-[55vw] max-w-xs bg-white rounded-xl"
+      >
+        <div className="row1  justify-center border-b">
+          <div
+            onClick={() => {
+              show(!showLikeClick);
+            }}
+            className=" float-right m-2 cursor-pointer "
+          >
+            <Close />
+          </div>
+          <p className=" mx-28 py-2 font-bold">Likes</p>
+        </div>
+
+        <div>
+          {likes.map((like, index) => (
+            <div
+              key={index}
+              className="likes flex justify-center items-center my-2"
+            >
+              <div className="photo mx-2">
+                <img
+                  className=" h-[40px] w-[40px] object-cover rounded-full"
+                  src={like?.user?.profile?.profilephoto}
+                  alt="sdd"
+                />
+              </div>
+              <div className="info">
+                <p className=" text-sm font-bold">{like?.user?.username}</p>
+                <p className=" text-sm text-gray-500">{like?.user?.username}</p>
+              </div>
+              <div className="button py-1 px-3 bg-gray-200 rounded-xl font-bold mx-2">
+                <button>Following</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+export default LikeList;
