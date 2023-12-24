d1e428252040492d9aec4c8d20b02888054fc7f9
jaimin1503 <123434935+jaimin1503@users.noreply.github.com>
2023-12-24 09:03:31 +0530

    search user done

--- src/components/SearchCompo.jsx
+++ src/components/SearchCompo.jsx
@@ -1,58 +1,102 @@
 import { Search, Close } from "@mui/icons-material";
-import { useState } from "react";
-import { useSelector } from "react-redux";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import Spinner from "../components/Spinner";
+import { IonItem, IonSpinner } from "@ionic/react";
 
 const SearchCompo = () => {
   const [inputValue, setInputValue] = useState("");
   const [suggestions, setSuggestions] = useState([]);
-  const { user } = useSelector((state) => state.user);
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    // Fetch all users once when the component mounts
+    setLoading(true);
+    axios
+      .get(`http://localhost:5555/user/getalluser`, {
+        withCredentials: true,
+      })
+      .then((res) => {
+        setUsers(res.data.alluser);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error(error);
+        setLoading(false);
+      });
+  }, []); // This runs only once on component mount
 
   const handleInputChange = (event) => {
     const value = event.target.value;
     setInputValue(value);
 
-    const usernames = [
-      "Ashish007",
-      "ranjeet_61",
-      "jaimin_15.3",
-      "chirag__25.10",
-      "hardik_18",
-      "daxil_10",
-      "jatu_31",
-    ];
-
-    const filteredSuggestions = usernames.filter((suggestion) =>
-      suggestion.toLowerCase().includes(value.toLowerCase())
+    // Filter users locally instead of making an API call on each change
+    const filteredUsers = users.filter(
+      (user) =>
+        value && user.username.toLowerCase().includes(value.toLowerCase())
     );
-    setSuggestions(filteredSuggestions);
+    setSuggestions(filteredUsers);
   };
 
   return (
-    <div className="container absolute top-[50%] left-[50%] w-full h-full translate-x-[-50%] translate-y-[-50%] bg-gray-200 ">
-      <input
-        type="text"
-        className=" w-full rounded-lg p-2 outline-none absolute pl-10"
-        value={inputValue}
-        onChange={handleInputChange}
-        placeholder="Search..."
-      />
-      <div className=" flex justify-between">
-        <div className=" relative p-2 cursor-pointer w-[25px] h-[25px]">
-          <Search />
+    <>
+      {loading && <Spinner />}
+      <div className="container absolute top-[50%] left-[50%] w-full h-full translate-x-[-50%] translate-y-[-50%] bg-gray-50 ">
+        <div className="h1 text-2xl p-5 font-semibold">Search</div>
+        <div className=" flex flex-col">
+          <input
+            type="text"
+            className="w-full rounded-lg p-2 outline-none absolute pl-10 bg-gray-100 my-2"
+            value={inputValue}
+            onChange={handleInputChange}
+            placeholder="Type to Search..."
+          />
+          <div className="flex justify-between">
+            <div className="relative p-2 cursor-pointer w-[25px] h-[25px] my-2">
+              <Search />
+            </div>
+            {!loading && (
+              <div
+                onClick={() => setInputValue("")}
+                className="relative p-2 cursor-pointer float-right text-gray-500 my-2"
+              >
+                <Close />
+              </div>
+            )}
+            {loading && (
+              <div className="p-2">
+                <IonItem>
+                  <IonSpinner name="lines-sharp-small"></IonSpinner>
+                </IonItem>
+              </div>
+            )}
+          </div>
         </div>
-        <div
-          onClick={() => setInputValue("")}
-          className=" relative p-2 cursor-pointer float-right text-gray-500"
-        >
-          <Close />
-        </div>
+        <ul className="mt-2 mx-5">
+          {suggestions.map((user) => (
+            <div key={user._id} className=" py-2">
+              <div className=" flex items-center">
+                <div className="profile_pic">
+                  <img
+                    className=" h-[36px] w-[36px] object-cover rounded-full mx-2"
+                    src={user?.profile?.profilephoto}
+                    alt="profilepic"
+                  />
+                </div>
+                <div className="userinfo">
+                  <p className="">{user?.username}</p>
+                  <p className=" text-gray-400 text-xs">
+                    {user?.profile?.profilename}
+                  </p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </ul>
       </div>
-      <ul className=" mt-5">
-        {suggestions.map((suggestion, index) => (
-          <li key={index}>{suggestion}</li>
-        ))}
-      </ul>
-    </div>
+    </>
   );
 };
 export default SearchCompo;
