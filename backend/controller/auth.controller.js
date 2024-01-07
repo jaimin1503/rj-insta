6c471bdf6a43a572e522dd4c35825e4061cfda31
Ranjeet0062 <105407521+Ranjeet0062@users.noreply.github.com>
2024-01-07 08:37:36 +0530

    Update auth.controller.js

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -142,7 +142,7 @@ export const login = async (req, res) => {
 
     const options = {
       // expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
-      httpOnly: true,
+      httpOnly: false,
       withCredentials: true,
     };
 
