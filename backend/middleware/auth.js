ef3ba571463a70e9858bf487a2685b78ac24391a
jaimin1503 <123434935+jaimin1503@users.noreply.github.com>
2024-01-08 09:09:08 +0530

    Update auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,34 +1,32 @@
 import dotenv from "dotenv";
 import jwt from "jsonwebtoken";
 dotenv.config();
-
 export const auth = (req, res, next) => {
   try {
-    const token =
-      req.body.token || req.cookies.token ||  req.headers.Authorization?.split(" ")[1];
-
+    const token = req.body.token || req.cookies.token;
     if (!token) {
       return res.status(401).json({
         success: false,
-        message: "Token missing",
+        message: "token missing",
       });
     }
-
     try {
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      req.user = decoded;
-      next();
+      const decode = jwt.verify(token, process.env.JWT_SECRET);
+
+      req.user = decode;
     } catch (e) {
       return res.status(401).json({
         success: false,
-        message: "Token is invalid",
+        message: "token is invalid",
       });
     }
+
+    next();
   } catch (err) {
-    console.error(err);
+    console.log(err);
     return res.status(401).json({
       success: false,
-      message: "Something went wrong while verifying the token",
+      message: "Something went wrong while verifying token",
     });
   }
 };
