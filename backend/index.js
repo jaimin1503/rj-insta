ecc8bd344bff99acef212d5046bee2c176301462
Ranjeet0062 <odedararanjeet1361@gmail.com>
2024-04-05 15:27:19 +0530

    cors set

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://friends-flock.vercel.app/","http://localhost:5173"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
@@ -50,7 +50,7 @@ const server = app.listen(port, () => {
 const io = new Server(server, {
   pingTimeout: 60000,
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: ["https://friends-flock.vercel.app/","http://localhost:5173"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   },
